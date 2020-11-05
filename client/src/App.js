8ee080a129835a23cf3d4bacd988a3704099fd06
shariarfaisal <faisaljr623@gmail.com>
2020-11-05 22:05:01 +0600

    fresh env

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,8 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from './pages/Home'
-import BaseContextProvider from './contexts/BaseContext'
 
 const App = (props) => {
   return(
-    <Router>
-      <Switch>
-        <BaseContextProvider>
-          <Route path="/" component={Home} />
-        </BaseContextProvider>
-      </Switch>
-    </Router>
+    <div>hello world</div>
   )
 }
 export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './sass/style.scss'
-import axios from 'axios'
-
-axios.defaults.baseURL = 'https://faisal-e-backend.herokuapp.com/'
-
-const token = localStorage.getItem('x-user-token')
-if(token){
-  axios.defaults.headers['Authorization'] = token
-}
 
 ReactDOM.render(
   <App />,
